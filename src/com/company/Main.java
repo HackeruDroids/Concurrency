99a7feba32e1a1b22c6e52489b4d57b3e54e9b03
hackerudroids <tomerjava@gmail.com>
2017-03-23 21:33:56 +0200

    Executors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,39 @@
 package com.company;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
-        SheepCounter counter = new SheepCounter();
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
+        SheepCounter sheepCounter = new SheepCounter();
         for (int i = 0; i < 10; i++) {
-            new Thread(()->counter.incrementAndPrint()).start();
+            executor.submit(() -> {
+                sheepCounter.incrementAndPrint();
+            });
         }
+
+        executor.shutdown(); //Now();
+
+        //
+
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(4);
+        scheduledThreadPool.schedule(()->{}, 1, TimeUnit.SECONDS);
+
+        scheduledThreadPool.scheduleAtFixedRate(() -> {
+            System.out.println(1);
+            System.out.println(1);
+            System.out.println(1);
+            System.out.println(1);
+            System.out.println(1);
+            System.out.println(1);
+        }, 1, 1, TimeUnit.SECONDS);
+
+
+
     }
 }
--- src/com/company/SheepCounter.java
+++ src/com/company/SheepCounter.java
@@ -8,16 +8,17 @@ public class SheepCounter {
     final private Object lock1 = new Object();
 
     public void incrementAndPrint() {
-
         System.out.println("inside");
-
         synchronized (lock1) {
-            System.out.print(++counter + " ");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print(++counter + " \n");
         }
-
         System.out.println("Outside");
     }
-
 
     public void doSomething() {
         synchronized (lock1) {
